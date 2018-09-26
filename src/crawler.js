54140c319efbff19e134b2ad51832ceec0ff5b51
Oleg Shalygin <oshalygin@gmail.com>
2018-09-26 00:43:47 -0700

    Add error handling for required queryString params

--- src/crawler.js
+++ src/crawler.js
@@ -3,9 +3,30 @@ import express from 'express';
 const application = express();
 
 export const getPollingLocation = async (request, response) => {
-  const { address } = request.query;
-  console.log(address);
-  console.log('Hello');
+  const { lastName, birthDate, houseNumber, zipCode } = request.query;
+  if (!lastName) {
+    return response
+      .status(400)
+      .json('The queryString parameter [lastName] cannot be empty');
+  }
+
+  if (!birthDate) {
+    return response
+      .status(400)
+      .json('The queryString parameter [birthDate] cannot be empty');
+  }
+
+  if (!houseNumber) {
+    return response
+      .status(400)
+      .json('The queryString parameter [houseNumber] cannot be empty');
+  }
+
+  if (!zipCode) {
+    return response
+      .status(400)
+      .json('The queryString parameter [zipCode] cannot be empty');
+  }
 
   try {
     return response.status(200).json({});
